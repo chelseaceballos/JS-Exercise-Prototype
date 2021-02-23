714ab102438a213e2479dc0ca8299fb6d3b1b7b9
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-23 13:59:39 -0800

    completed task 1

--- index.js
+++ index.js
@@ -29,12 +29,16 @@ function Airplane(name) {
   /*
     TASK 1
       - Write a Person Constructor that initializes `name` and `age` from arguments. --> Name & Age inside function Parameters
+
       - All instances of Person should initialize with an empty `stomach` array. --> create stomach array, empty
+
       - Give instances of Person the ability to `.eat("someFood")`: --> Person.prototype.eat = function (){ this.eat = 10 items}
           + When eating an edible, it should be pushed into the `stomach`.
           + The `eat` method should have no effect if there are 10 items in the `stomach`.
-      - Give instances of Person the ability to `.poop()`: --> Person.prototype.poop = function () { this.poop = 'empty'}
+
+      - Give instances of Person the ability to `.poop()`: --> Person.prototype.poop = function () { this.poop = 'empty'
           + When an instance poops, its `stomach` should empty.
+
       - Give instances of Person a method `.toString()`: ---> Person.prototype.toString = function () {${name} , ${age}}
           + It should return a string with `name` and `age`. Example: "Mary, 50"
 
@@ -42,22 +46,33 @@ function Airplane(name) {
   */
   
  function Person(name, age) {
-    // const stomach = [] ; {
-    //   Person.prototype.eat = function () {
-    //     this.eat = 10;
-    //   } ;
-    //   Person.prototype.poop = function () {
-    //     this.poop = 'empty';
-    //   } ;
-    //   Person.prototype.toString = function (){
-    //     this.toString = (${name} , ${age})
-    //   }
-    // }
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
   }
-//  console.log(Person);
+
+
+// create an eat method, pass in argument of something edible, that edible thing should be pushed into the stomach
+// if the stomach has 10 items, person cannot eat anymore
+
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+//make person poop
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
  
 
-  
+  //make a person toString
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person('Chelsea', 24);
   
   
   
@@ -82,16 +97,31 @@ function Airplane(name) {
   
   /*
     TASK 3
-      - Write a Baby constructor subclassing Person.
-      - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-      - Besides the methods on Person.prototype, babies have the ability to `.play()`:
-          + Should return a string "Playing with x", x being the favorite toy.
+      - Write a Baby constructor sub classing Person. --> call person pass in this and attributes
+
+      - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`. --> create special attribute for favoriteToy
+
+      - Besides the methods on Person.prototype, babies have the ability to `.play()`: -->
+
+          + Should return a string "Playing with x", x being the favorite toy. -->
   */
- function Baby() {
-   
+ function Baby(attributes) {
+   Person.call(this, attributes)
+   this.favoriteToy = attribute.favoriteToy; //this is a special attribute that belongs only to BABY
   }
- 
-  
+  //linking to Person prototype with Baby prototype
+//  Baby.prototype = object.create(Person.prototype);
+ // Give Baby ability to play
+  // Baby.prototype.play = function (){
+  //   console.log(`Playing with ${this.favoriteToy}`);
+  // }
+
+  // const person = new Baby{
+  //   name: 'Baby Michael',
+  //   eat: 10,
+  //   poop: 'empty',
+  //   favoriteToy: 'ball',
+  // }
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
